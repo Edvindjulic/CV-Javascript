7b2fd7694907bfe0eb30ffd69c39a7445ff73518
edvindjulic <djulicedvin@gmail.com>
2023-01-09 19:44:40 +0100

    Updated media query for 360px + updated JS

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    clickButton();
+    loadButtonForResumePage();
 }
 
-function clickButton() {
+/**
+ * Function that loads the button for JS
+ */
+function loadButtonForResumePage() {
     const button = document.getElementById('button');
-    button.addEventListener('click', openPage);
+    button.addEventListener('click', loadResumePage);
 }
-
-function openPage() {
+/**
+ * When the button is clicked, it opens the resume page
+ */
+function loadResumePage() {
     window.location.href = './about.html'
 }
